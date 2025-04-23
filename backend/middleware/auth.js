82f9e87e920ebec8c4bca59709814fe3da2b149b
RiverForo Admin <admin@riverforo.com>
2025-04-22 21:17:26 -0400

    Fix: add complete protect middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,28 @@
-// backend/middleware/auth.js
+const jwt = require('jsonwebtoken');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('./async');
+const User = require('../models/User');
 
-const jwt = require("jsonwebtoken");
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-module.exports = function (req, res, next) {
-  const token = req.header("Authorization");
+  // Get token from Authorization header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
 
   if (!token) {
-    return res.status(401).json({ message: "No token, authorization denied" });
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
-    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
-    req.user = decoded;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    res.status(401).json({ message: "Token is not valid" });
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
-};
+});
